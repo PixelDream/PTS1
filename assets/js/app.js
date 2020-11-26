17711623fe87890a3dc68f42cbf41b3a9f8c1e92
Morgan NEHDI <yannaro35@gmail.com>
2020-11-26 15:08:55 +0100

    Remove developpeurs part

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 (() => {
-    
     const hamburger = {
         navToggle: document.querySelector('.nav-toggle'),
         nav: document.querySelector('nav'),
@@ -11,25 +10,4 @@
     };
 
     hamburger.navToggle.addEventListener('click', (e) => hamburger.doToggle(e));
-
-    try {
-        const card = {
-            techList: [...document.querySelector('.card__tech')?.children],
-            activeTech: 'android',
-            doToggle: function (e) {
-                e.preventDefault();
-                let el = e.target.parentElement;
-                document.getElementsByClassName('active__item')[0].classList.remove('active__item');
-                this.activeTech = el.dataset.item;
-                document.getElementsByClassName('card__active')[0].classList.remove('card__active');
-                el.classList.add('active__item');
-                document.getElementById(this.activeTech).classList.add('card__active');
-            }
-        }
-
-        card.techList.map((el) => el.addEventListener('click', (e) => card.doToggle(e)));
-    } catch (error) {
-        //Not in the page
-    }
-
 })();
